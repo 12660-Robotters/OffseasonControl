25a2106d8c4f6c4119bf6bb3cec07b44109fd1da
foehammer127 <foehammer127@gmail.com>
2023-02-11 20:59:31 -0600

    Add Op Modes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Auto.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.Robot.RobotContainer;
+
+@Autonomous
+public class Auto extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        RobotContainer robotContainer = new RobotContainer(hardwareMap, RobotContainer.OpModeType.AUTO, gamepad1, gamepad2);
+
+        while (opModeIsActive() && !isStopRequested()) {
+            robotContainer.run();
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Teleop.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.teamcode.Robot.RobotContainer;
+
+public class Teleop extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        RobotContainer robotContainer = new RobotContainer(hardwareMap, org.firstinspires.ftc.teamcode.Robot.RobotContainer.OpModeType.TELEOP, gamepad1, gamepad2);
+
+        while (opModeIsActive() && !isStopRequested()) {
+            robotContainer.run();
+        }
+
+        robotContainer.reset();
+    }
+}
