3b5f31d7c80b20954397a13caa70f546c138dcf1
foehammer127 <foehammer127@gmail.com>
2023-02-11 19:52:39 -0600

    Added a Basic Command and added Subsystem To Robot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/RobotContainer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/RobotContainer.java
@@ -1,7 +1,47 @@
 package org.firstinspires.ftc.teamcode.Robot;
 
 
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.teamcode.Robot.Subsystems.Claw_Subsystem;
 import org.firstinspires.ftc.teamcode.util.Robot;
 
 public class RobotContainer extends Robot {
+
+    GamepadEx driverone;
+    GamepadEx drivertwo;
+    public enum OpModeType {
+        TELEOP, AUTO
+    }
+
+    Claw_Subsystem claw_subsystem;
+
+    public RobotContainer(HardwareMap hardwareMap, OpModeType type, Gamepad driverone, Gamepad drivertwo) {
+        claw_subsystem = new Claw_Subsystem(hardwareMap);
+
+
+        switch (type) {
+            case AUTO:
+                initAuto();
+
+            case TELEOP:
+
+                this.driverone = new GamepadEx(driverone);
+                this.drivertwo = new GamepadEx(drivertwo);
+                initTeleop();
+
+
+        }
+
+    }
+
+    public void initAuto() {
+
+    }
+
+    public void initTeleop() {
+
+    }
 }
