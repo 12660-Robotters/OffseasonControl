860534c73239d93a6d8eb484867e9a299eecf5a5
cheeetoo <finnyjc12@gmail.com>
2023-02-27 16:20:39 -0600

    added bulkreadingclear command

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/commands/BulkReadingClear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/commands/BulkReadingClear.java
@@ -1,2 +1,21 @@
-package org.firstinspires.ftc.teamcode.Robot.commands;public class BulkReadingClear {
+package org.firstinspires.ftc.teamcode.Robot.commands;
+
+import com.arcrobotics.ftclib.command.CommandBase;
+import com.qualcomm.hardware.lynx.LynxModule;
+
+import org.firstinspires.ftc.teamcode.Robot.Subsystems.BulkReading;
+
+public class BulkReadingClear extends CommandBase {
+    private final BulkReading bulkReading;
+
+    public BulkReadingClear(BulkReading bulkReading) {
+        this.bulkReading = bulkReading;
+    }
+
+    @Override
+    public void execute() {
+        for (LynxModule module : bulkReading.getAllHubs()) {
+            module.clearBulkCache();
+        }
+    }
 }
